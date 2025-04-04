e5e6bf21722b5f007e7b7d3c1f3557accb0cb1e0
Your Name <you@example.com>
2025-04-04 16:31:14 +0300

    added buttons and DB for films

--- src/functions_for_bot.py
+++ src/functions_for_bot.py
@@ -1,3 +1,37 @@
 import telebot
 from telebot import types
+import csv
+import random
+
 token = '8065253218:AAHjv0WTW5qv2xp9tsT9EhXGXwqrnomXXpM'
+bot = telebot.TeleBot(token)
+
+def random_film():
+    film = 'none'
+    with open('src/films.csv') as f:
+        reader = csv.reader(f)
+        ind_reader = 0
+        film_ind = random.randint(0, 4804)
+        for row in reader:
+            if ind_reader == film_ind:  
+                film = row[1]
+                break
+            ind_reader += 1
+    print(film)
+    return film
+
+# random_film()
+
+def main_b():
+    buttons = types.InlineKeyboardMarkup()   
+    buttons.add(types.InlineKeyboardButton(text='Случайный фильм', callback_data='random_film'), types.InlineKeyboardButton(text='Выбор по категории', callback_data='user_choose'))
+    return buttons
+
+def random_film_b():
+    buttons = types.InlineKeyboardMarkup()   
+    buttons.add( types.InlineKeyboardButton(text='Выбрать другой фильм', callback_data='random_film'), types.InlineKeyboardButton(text='Главное меню', callback_data='main'))
+    return buttons  
+def user_choose_b():
+    buttons = types.InlineKeyboardMarkup()   
+    buttons.add(types.InlineKeyboardButton(text='Главное меню', callback_data='main'))
+    return buttons    
--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
 from functions_for_bot import *
 # import math
 
-bot = telebot.TeleBot(token)
+# bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.from_user.id, 
-                     "Привет)\nБот предназначен для подбора фильма по категориям")
+                     "Привет)\nБот предназначен для подбора фильма по категориям", reply_markup=main_b())
 @bot.message_handler(commands=['help'])
 def start_message(message):
     bot.send_message(message.from_user.id, "Что бы начать работу с ботом введите команду /start")
+
+@bot.callback_query_handler(func=lambda call: True)    
+def call_back(call):
+    if call.data == 'main':
+        bot.send_message(chat_id=call.message.chat.id, text="тестим кнопки\n"
+       "Выберите, каким способом подобрать фильм", reply_markup=main_b())       
+    if call.data == 'random_film':   
+        film = random_film() 
+        bot.send_message(chat_id=call.message.chat.id, text=f"Ваш фильм: {film}", reply_markup=random_film_b())
+    if call.data == 'user_choose':   
+        bot.send_message(chat_id=call.message.chat.id, text=f"Пока в разработке...", reply_markup=user_choose_b())
+
+  
 
 bot.polling(none_stop=True, interval=0)
