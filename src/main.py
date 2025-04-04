49ee7727fa8bbe095ffab246d6fff6a8aecd6542
Your Name <you@example.com>
2025-04-04 17:24:12 +0300

    replies for stickers and text from user

--- src/main.py
+++ src/main.py
@@ -8,6 +8,15 @@ def start_message(message):
 def start_message(message):
     bot.send_message(message.from_user.id, "Что бы начать работу с ботом введите команду /start")
 
+@bot.message_handler(content_types=['sticker'])
+def start_message(message):
+        bot.send_message(message.from_user.id, "Забавный стикер 😁")
+
+@bot.message_handler()
+def start_message(message):
+        bot.send_message(message.from_user.id, "К сожалению, я не умею читать, но я уверен, что там что-то хорошее😜")
+
+
 @bot.callback_query_handler(func=lambda call: True)    
 def call_back(call):
     if call.data == 'main':
