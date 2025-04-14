8bbd90f60532a8dc9192e89cca000366a937f168
farlenwa@student.21-school.ru <farlenwa@student.21-school.ru>
2025-04-15 00:57:40 +0300

    new fortmat of random films + start of selections

--- src/functions_for_bot.py
+++ src/functions_for_bot.py
@@ -2,23 +2,44 @@ import telebot
 from telebot import types
 import csv
 import random
+import hashlib
 
 token = '8065253218:AAHjv0WTW5qv2xp9tsT9EhXGXwqrnomXXpM'
 bot = telebot.TeleBot(token)
+callback_map = {}
+buttons_per_page = 15  # 5 строки по 3 кнопок
+
+def short_callback(value: str) -> str:
+    # Генерируем короткий уникальный ключ (12 символов)
+    return hashlib.md5(value.encode()).hexdigest()[:12]
+
+#funcrion for updating genres
+def check_genres():
+    genre = []
+    with open('src/kinopoisk_top250_full.csv', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in (reader):
+            # print(row['genres'])
+            row_genre = row['genres'].split(',')
+            for i in row_genre:
+                if not(i in genre):
+                    genre.append(i)
+    f.close()
+    return genre
+
+genre = check_genres()
 
 def random_film():
-    film = 'none'
-    with open('src/films.csv') as f:
+    film = ""
+    with open('src/kinopoisk_top250_full.csv', encoding='utf-8') as f:
         reader = csv.reader(f)
-        ind_reader = 0
-        film_ind = random.randint(0, 4804)
-        for row in reader:
-            if ind_reader == film_ind:  
-                film = row[1]
+        film_ind = random.randint(1, 250)
+        for ind_reader, row in enumerate(reader):
+            if ind_reader == film_ind:
+                # print(row[0])
+                film = row
                 break
-            ind_reader += 1
     f.close()
-    print(film)
     return film
 
 def del_prev_buttons(call):
@@ -26,11 +47,77 @@ def del_prev_buttons(call):
 
 def main_b():
     buttons = types.InlineKeyboardMarkup()   
-    buttons.add(types.InlineKeyboardButton(text='Случайный фильм', callback_data='random_film'), types.InlineKeyboardButton(text='⛔️Выбор по категории', callback_data='user_choose'))
+    buttons.add(types.InlineKeyboardButton(text='Случайный фильм', callback_data='random_film'), 
+                types.InlineKeyboardButton(text='⛔️Выбор по категории', callback_data='user_choose'),
+                types.InlineKeyboardButton(text='Подборки', callback_data='selection_menu'))
     return buttons
+
+def selection_menu_b():
+    buttons = types.InlineKeyboardMarkup()       
+    buttons.add(types.InlineKeyboardButton(text='По жанрам', callback_data='sel_genre'), 
+                types.InlineKeyboardButton(text='По актерам', callback_data='sel_actor'),
+                types.InlineKeyboardButton(text='По периоду выпуска', callback_data='sel_year'),
+                types.InlineKeyboardButton(text='Все фильмы', callback_data='sel_all_films'),
+                types.InlineKeyboardButton(text='Все актеры', callback_data='sel_all_actor'),
+                types.InlineKeyboardButton(text='Главное меню', callback_data='main')              
+                )   
+    return buttons
+
+
+def selection_b(type_selection, page=0):
+    types_of_sets = {
+        'sel_genre': [10, 'genres'],
+        'sel_actor': [11, 'actors'],
+        'sel_year': [0, 'year'],
+        'sel_all_films': [-1, 'none'],
+        'sel_all_actor': [-11, 'actors']
+    }
+
+    markup = types.InlineKeyboardMarkup(row_width=3)
+
+    if types_of_sets[type_selection][0] > 0:
+        unique = set()
+        buttons_data = []
+
+        with open('src/kinopoisk_top250_full.csv', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                values = row[types_of_sets[type_selection][1]].split(',')
+                for val in values:
+                    val = val.strip()
+                    if val and val not in unique:
+                        unique.add(val)
+                        cb_data = short_callback(val)
+                        callback_map[cb_data] = val
+                        buttons_data.append((val, cb_data))
+
+        total_pages = (len(buttons_data) + buttons_per_page - 1) // buttons_per_page
+        start = page * buttons_per_page
+        end = start + buttons_per_page
+        current_buttons = buttons_data[start:end]
+
+        for i in range(0, len(current_buttons), 3):
+            row = [
+                types.InlineKeyboardButton(text=name, callback_data=cb_data)
+                for name, cb_data in current_buttons[i:i+3]
+            ]
+            markup.row(*row)
+
+        # Кнопки навигации
+        nav_buttons = [types.InlineKeyboardButton(text='Главное меню', callback_data='main')]
+        if page > 0:
+            nav_buttons.append(types.InlineKeyboardButton("⏮ Назад", callback_data=f"{type_selection}_page_{page-1}"))
+        if page < total_pages - 1:
+            nav_buttons.append(types.InlineKeyboardButton("⏭ Вперёд", callback_data=f"{type_selection}_page_{page+1}"))
+        if nav_buttons:
+            markup.row(*nav_buttons)
+
+    return markup
+ 
 def random_film_b():
     buttons = types.InlineKeyboardMarkup()   
-    buttons.add( types.InlineKeyboardButton(text='Выбрать другой фильм', callback_data='random_film'), types.InlineKeyboardButton(text='Главное меню', callback_data='main'))
+    buttons.add( types.InlineKeyboardButton(text='Выбрать другой фильм', callback_data='random_film'), 
+                types.InlineKeyboardButton(text='Главное меню', callback_data='main'))
     return buttons  
 def user_choose_b():
     buttons = types.InlineKeyboardMarkup()   
--- src/main.py
+++ src/main.py
@@ -17,16 +17,37 @@ def start_message(message):
 
 @bot.callback_query_handler(func=lambda call: True)    
 def call_back(call):
+    del_prev_buttons(call)
+    if "_page_" in call.data:
+            parts = call.data.split("_page_")
+            type_selection = parts[0]
+            page = int(parts[1])
+            bot.edit_message_reply_markup(
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                reply_markup=selection_b(type_selection, page)
+            )
+            return
     if call.data == 'main':
-        del_prev_buttons(call)
+        # del_prev_buttons(call)
         bot.send_message(chat_id=call.message.chat.id, text="тестим кнопки\n"
-       "Выберите, каким способом подобрать фильм", reply_markup=main_b())       
+       "Выберите, каким способом подобрать фильм", reply_markup=main_b())      
+    if call.data == 'selection_menu':
+        # del_prev_buttons(call)
+        bot.send_message(chat_id=call.message.chat.id, text="Вот текущие подборки:",reply_markup=selection_menu_b())
+    if call.data in ['sel_genre', 'sel_actor', 'sel_year', 'sel_all_actor', 'sel_all_films']:
+        setting = call.data 
+        bot.send_message(chat_id=call.message.chat.id, text="Выбор подборки фильма",reply_markup=selection_b(setting))
+    if call.data in callback_map:
+        real_value = callback_map[call.data]
+        bot.send_message(call.message.chat.id, f"Ты выбрал: {real_value}")
+
     if call.data == 'random_film':   
         film = random_film() 
-        del_prev_buttons(call)
-        bot.send_message(chat_id=call.message.chat.id, text=f"🎬Ваш фильм: `{film}`",parse_mode="MARKDOWN", reply_markup=random_film_b())
+        # del_prev_buttons(call)
+        bot.send_photo(chat_id=call.message.chat.id,photo=film[6] ,caption=f"🎬*Ваш фильм*: `{film[1]}`\n\n*Описание фильма*: {film[7]}\n\n *Возрастной рейтинг*: {film[15]}",parse_mode="MARKDOWN", reply_markup=random_film_b())
     if call.data == 'user_choose':   
-        del_prev_buttons(call)
+        # del_prev_buttons(call)
         bot.send_message(chat_id=call.message.chat.id, text=f"🫣Пока в разработке...", reply_markup=user_choose_b())
 
 bot.polling(none_stop=True, interval=0)
